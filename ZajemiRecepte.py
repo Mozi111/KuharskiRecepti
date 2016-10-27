7e27a819b195d17582f977d3698de556a6ecb94c
Mozi111 <Splintercell.savage@gmail.com>
2016-10-27 22:51:28 +0200

    Leto izracunano v pandas

--- ZajemiRecepte.py
+++ ZajemiRecepte.py
@@ -24,13 +24,11 @@ regex_recepta = re.compile(
           r'''(<br><br>Zadnja fotografija: \d{0,2}\.\d{0,2}\.\d{0,4})?'''
           r'''(<br>Število fotografij: \d*)?'''
           r'''.?>(?P<naslov>.*?)</a>.*?'''
-          #Zahtevnost
           r'''tezavnost'><img src='/grafika6/ikona-(?P<tezavnost1>.*?)\.png' alt='Zahtevnost' '''
           r'''title='zahtevnost'><img src='/grafika6/ikona-(?P<tezavnost2>.*?)\.png' alt='Zahtevnost' '''
           r'''title='zahtevnost'><img src='/grafika6/ikona-(?P<tezavnost3>.*?)\.png' alt='Zahtevnost' '''
           r'''title='zahtevnost'><img src='/grafika6/ikona-(?P<tezavnost4>.*?)\.png' alt='Zahtevnost' '''
           r'''title='zahtevnost'><img src='/grafika6/ikona-(?P<tezavnost5>.*?)\.png' alt='Zahtevnost'.*?'''
-          ####
           r'''(<span class='cas'>(?P<priprava>.*?)</span></p>.*?)?'''
           r'''(kolicina: (?P<kolicina>.*?)</p></div>.*?)?'''
           r'''(<img class='spol' src='.*?' title='(?P<spol>.*?)'.*?)?'''
@@ -49,6 +47,7 @@ def pocisti_recept(recept):
      podatki = recept.groupdict()
      podatki['id'] = int(podatki['id'])
      podatki['naslov'] = podatki['naslov'].strip()
+     podatki['objava'] = str(podatki['objava'])
      # Ce je priprava v minutah, shrani kot stevilo
      # Ce je priprava 4+ ur shrani kot besedilo
      if podatki['priprava']:
@@ -70,11 +69,8 @@ def pocisti_recept(recept):
      del podatki['tezavnost4']
      del podatki['tezavnost5']
      # Ce podatkov o bolj zdravi jedi ni
-     if not podatki['zdravajed']:
-          podatki['zdravajed'] = "Ni podano"
-     podatki['letoobjave'] = int(re.split(r'\.', podatki['objava'])[2])
-     if podatki['zadnjafoto']:
-        podatki['letofoto'] = int(re.split(r'\.', podatki['zadnjafoto'])[2])
+     #if not podatki['zdravajed']:
+     #     podatki['zdravajed'] = "Ni podano"
 
      podatki['avtor'] = {'id': podatki['idavtorja'], 'avtor': podatki['avtor'], 'spol': podatki['spol']}
      del podatki['idavtorja']
